d0b631ed0fec26f34e8b23bd8775ec61a6873158
BuildTools <unconfigured@null.spigotmc.org>
2023-06-26 23:58:13 -0300

    Version 1.4 button outline improved

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function toggleMode() {
 
 
 }
+
+const switchElement = document.getElementById('switch');
+const buttonElement = switchElement.querySelector('button');
+const spanElement = switchElement.querySelector('span');
+
+function handleOverlap() {
+  if (isOverlapping(buttonElement, spanElement)) {
+    buttonElement.classList.add('no-darken');
+    spanElement.classList.add('no-darken');
+  } else {
+    buttonElement.classList.remove('no-darken');
+    spanElement.classList.remove('no-darken');
+  }
+}
+
+function isOverlapping(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+
+  return (
+    rect1.left < rect2.right &&
+    rect1.right > rect2.left &&
+    rect1.top < rect2.bottom &&
+    rect1.bottom > rect2.top
+  );
+}
+
+window.addEventListener('resize', handleOverlap);
+handleOverlap();
